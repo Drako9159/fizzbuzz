46a0e973f71783b8e8ca57143488e95c314d7be2
Drako9159 <chochito9159@gmail.com>
2022-04-28 19:25:38 -0500

    add 3 passed test

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,21 +1,18 @@
 const ExplorerService = require("../lib/services/ExplorerService");
-const Reader = require("../lib/utils/reader.js"); 
-const explorers = Reader.readJsonFile("./explorers.json");
-
 describe("Tets fizzbuzz", () => {
-    test("test filterByMission", () => {
-        const totalExplorersInNode = ExplorerService.filterByMission(explorers, "node");
-        const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
-        expect(explorersInNode).toStrictEqual(totalExplorersInNode);
+    test("Test1: filterByMission", () => {
+        const fizzbuzzObjet = [{mission: "node"},{mission: "java"}] 
+        const ExplorersInNode = ExplorerService.filterByMission(fizzbuzzObjet, "node");
+        expect(ExplorersInNode).toEqual([{mission: "node"}]);
     });
-    test("test getAmountOfExplorersByMission", () => {
-        const totalExplorers = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
-        expect(totalExplorers).toBe(10);
+    test("Test2: getAmountOfExplorersByMission", () => {
+        const fizzbuzzObjet = [{mission: "node"},{mission: "java"}] 
+        const totalExplorersInNode = ExplorerService.getAmountOfExplorersByMission(fizzbuzzObjet, "node");
+        expect(totalExplorersInNode).toBe(1);
     });
-    test("test getExplorersUsernamesByMission", () => {
-        const usernamesInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
-        const usernamesByReader = explorers.filter((explorer) => explorer.mission == "node");
-        const newUsernames = usernamesByReader.map((explorer) => explorer.githubUsername);
-        expect(newUsernames).toStrictEqual(usernamesInNode);
+    test("Test3: getExplorersUsernamesByMission", () => {
+        const fizzbuzzObjet = [{mission: "node", githubUsername: "aline"},{mission: "java", githubUsername: "bestie"}] 
+        const usernamesInNode = ExplorerService.getExplorersUsernamesByMission(fizzbuzzObjet, "node");
+        expect(usernamesInNode).toEqual(["aline"]);
     });
 });
